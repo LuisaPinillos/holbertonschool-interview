d307015eef024209d9ca7749e78da6b3b59480a5
LuisaPinillos <luisapinillos20@gmail.com>
2022-08-24 12:32:07 -0500

    Fix code

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -10,14 +10,11 @@ def minOperations(n):
 
     if type(n) is not int or n < 2:
         return 0
-    else:
-        num_op = 0
 
-        for i in range(2, n+1):
-            while n % i == 0 and n != 0:
-                if n != 0:
-                    num_op += i
-                    n /= 2
-                else:
-                    num_op += i
-        return num_op
+    num_op = 0
+
+    for i in range(2, n):
+        while n % i == 0 and n != 0:
+            num_op += i
+            n /= 2
+    return num_op
