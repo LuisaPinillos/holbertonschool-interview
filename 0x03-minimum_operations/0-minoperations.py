59a3e0de02ab82b5746dff1fa2d82024c29ef486
LuisaPinillos <luisapinillos20@gmail.com>
2022-08-24 12:47:09 -0500

    Fix code

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -8,12 +8,12 @@ Returns an integer. If n is impossible to achieve, return 0.
 
 def minOperations(n):
 
-    if type(n) is not int or n <= 1:
+    if type(n) is not int or n < 2:
         return 0
 
     num_op = 0
 
-    for i in range(2, n):
+    for i in range(2, n+1):
         while n % i == 0 and n / i != 0:
             num_op += i
             n /= i
